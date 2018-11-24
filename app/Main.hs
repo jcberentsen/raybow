db00ced5cfff6e71c5062b2220fe448cf7f14271
Christian Berentsen <jc.berentsen@gmail.com>
2018-11-24 18:14:01 +0100

    Animate two pixel colors

--- app/Main.hs
+++ app/Main.hs
@@ -1,8 +1,35 @@
+{-# language OverloadedStrings #-}
 module Main where
 
 import Lib
 
+import Prelude hiding (concat)
 import Graphics.Gloss
+import Graphics.Gloss.Data.Bitmap
+import Data.ByteString (pack, concat)
+import Data.Bits
 
 main :: IO ()
-main = display (InWindow "Nice Window" (200, 200) (10, 10)) white (Circle 80)
+main = do
+
+  let window = InWindow "Raybow" (200,200) (10,400)
+  let background = greyN 0.4
+
+  -- TODO Adjust bitmap to window
+  -- window background picture
+  simulate window background 100 initialModel view step
+
+initialModel = 0
+
+view x =
+  let inv = 255 - x
+      bs = (concat . map pack) $
+        [ [0, x, x, 255]
+        , [0, inv, inv, 255]
+        ]
+      bitmapFormat = BitmapFormat TopToBottom PxRGBA
+      rendering = bitmapOfByteString 2 1 bitmapFormat bs True
+  in
+    scale 100 200 rendering
+
+step = const (const (\x -> (x + 1) .&. 255))
